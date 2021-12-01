2ce3a96f569c9d7ad58a0a8d68de342125553a01
Carlos Lezama <carloselezamaj@gmail.com>
2021-11-30 21:39:43 -0600

    Add hw2 solutions

--- hw2/src.R
+++ hw2/src.R
@@ -39,9 +39,29 @@ time.1 <- system.time({
                 filter(Year == i,
                        ProductDescription == j)
             
+            if (dim(imports)[1] == 0) {
+                imports <- imports %>%
+                    add_row(
+                        ProductDescription = j,
+                        TradeFlowCode = 5,
+                        TradeValue.in.1000.USD = 0,
+                        Year = i
+                    )
+            }
+            
             exports <- ALL3.exports %>%
                 filter(Year == i,
                        ProductDescription == j)
+            
+            if (dim(exports)[1] == 0) {
+                exports <- exports %>%
+                    add_row(
+                        ProductDescription = j,
+                        TradeFlowCode = 6,
+                        TradeValue.in.1000.USD = 0,
+                        Year = i
+                    )
+            }
             
             diff <- abs(imports$TradeValue.in.1000.USD -
                             exports$TradeValue.in.1000.USD)
@@ -56,7 +76,7 @@ time.1 <- system.time({
                         prod = j,
                         ratio = ratio)
             
-            cat(i, j, '\n')
+            # cat(i, j, '\n')
         }
     }
 })
@@ -68,9 +88,29 @@ time.2 <- system.time({
                 filter(Year == i,
                        ProductDescription == j)
             
+            if (dim(imports)[1] == 0) {
+                imports <- imports %>%
+                    add_row(
+                        ProductDescription = j,
+                        TradeFlowCode = 5,
+                        TradeValue.in.1000.USD = 0,
+                        Year = i
+                    )
+            }
+            
             exports <- ALL5.exports %>%
                 filter(Year == i,
                        ProductDescription == j)
+            
+            if (dim(exports)[1] == 0) {
+                exports <- exports %>%
+                    add_row(
+                        ProductDescription = j,
+                        TradeFlowCode = 6,
+                        TradeValue.in.1000.USD = 0,
+                        Year = i
+                    )
+            }
             
             diff <- abs(exports$TradeValue.in.1000.USD -
                             imports$TradeValue.in.1000.USD)
@@ -85,13 +125,16 @@ time.2 <- system.time({
                         prod = j,
                         ratio = ratio)
             
-            cat(i, j, '\n')
+            # cat(i, j, '\n')
         }
     }
 })
 
-time.1
-time.2
+# time.1
+# time.2
+
+trade.ALL3 <- na.omit(trade.ALL3)
+trade.ALL5 <- na.omit(trade.ALL5)
 
 gl.ALL3 <- aggregate(. ~ year, trade.ALL3[-2], mean)
 gl.ALL3$index <- 1 - gl.ALL3$ratio
@@ -99,23 +142,23 @@ gl.ALL3$index <- 1 - gl.ALL3$ratio
 gl.ALL5 <- aggregate(. ~ year, trade.ALL5[-2], mean)
 gl.ALL5$index <- 1 - gl.ALL5$ratio
 
-colours <- c('Three-digit SITC' = '#1e40ca',
-             'Five-digit SITC' = '#00a2ed')
-
-ggplot() +
-    geom_line(aes(
-        x = gl.ALL3$year,
-        y = gl.ALL3$index,
-        colour = 'Three-digit SITC'
-    )) +
-    geom_line(aes(
-        x = gl.ALL5$year,
-        y = gl.ALL5$index,
-        colour = 'Five-digit SITC'
-    )) +
-    theme_minimal() +
-    labs(title = 'Grubel and Lloyd Index Weighted Average',
-         x = 'year',
-         y = NULL) +
-    scale_colour_manual(name = '', values = colours) +
-    scale_x_continuous(breaks = seq(1990, 2010, 2))
+# colours <- c('Three-digit SITC' = '#1e40ca',
+#              'Five-digit SITC' = '#00a2ed')
+# 
+# ggplot() +
+#     geom_line(aes(
+#         x = gl.ALL3$year,
+#         y = gl.ALL3$index,
+#         colour = 'Three-digit SITC'
+#     )) +
+#     geom_line(aes(
+#         x = gl.ALL5$year,
+#         y = gl.ALL5$index,
+#         colour = 'Five-digit SITC'
+#     )) +
+#     theme_minimal() +
+#     labs(title = 'Grubel and Lloyd Index Weighted Average',
+#          x = 'year',
+#          y = NULL) +
+#     scale_colour_manual(name = '', values = colours) +
+#     scale_x_continuous(breaks = seq(1990, 2010, 2))
